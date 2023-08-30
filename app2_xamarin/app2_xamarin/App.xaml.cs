74e081ea3ad819b4bbeaf4738b871969121a2ac5
undotita <leoso@DESKTOP-LVAJHK5>
2023-08-30 10:55:03 -0500

    un commit

---
+++ app2_xamarin/app2_xamarin/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using app2_xamarin.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app2_xamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new PersonaPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ app2_xamarin/app2_xamarin/Servicio/PersonaServicio.cs
@@ -0,0 +1,48 @@
+﻿using app2_xamarin.model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace app2_xamarin.Servicio
+{
+    public class PersonaServicio
+    {
+        public ObservableCollection<PersonaModel> personas { get; set; }
+
+        public PersonaServicio()
+        {
+            if (personas == null)
+            {
+                personas = new ObservableCollection<PersonaModel>();
+            }
+        }
+
+        public ObservableCollection<PersonaModel> Consultar()
+        {
+            return personas;
+        }
+
+        public void Guardar(PersonaModel modelo) {
+        personas.Add(modelo);
+        
+        }
+
+        public void Modificar(PersonaModel modelo)
+        {
+            for(int i = 0; i < personas.Count; i++) { 
+                if (personas[i].Id == modelo.Id)
+                {
+                    personas[i] = modelo;
+                }
+            }
+        }
+
+        public void Eliminar(string idpersona)
+        {
+            PersonaModel modelo = personas.FirstOrDefault(p=> p.Id == idpersona);
+            personas.Remove(modelo);
+        }
+    }
+}
